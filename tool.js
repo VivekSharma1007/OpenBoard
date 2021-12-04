78cb2dd66e596b0c386ff7d40e9939a8d1c33968
VivekSharma1007 <viveksharma10.in@gmail.com>
2021-12-04 14:49:35 +0530

    fixed-remove

--- tool.js
+++ tool.js
@@ -1,7 +1,6 @@
 let toolsContainer = document.querySelector(".tool-options-icons-container");
 let optionContainer = document.querySelector(".option-icon");
 let optionFlag = true;
-let idNo = 0;
 let upload = document.querySelector(".upload");
 let toolPencil = document.querySelector(".tool-pencil");
 let toolEraser = document.querySelector(".tool-eraser");
@@ -10,6 +9,7 @@ let eraserIcon = document.querySelector(".eraser");
 let stickyIcon = document.querySelector(".sticky");
 let pencilFlag = false;
 let eraserFlag = false;
+let stickyFlag = true;
 
 
 optionContainer.addEventListener("click", (e) => {
@@ -72,62 +72,47 @@ eraserIcon.addEventListener("click", (e) => {
 
 
 stickyIcon.addEventListener("click", (e) => {
-    let toolSticky = document.createElement("div");
-    toolSticky.setAttribute("class", "tool-sticky");
-    idNo++;
-    toolSticky.setAttribute("id", "id"+idNo);
-    toolSticky.innerHTML = `
-        <div class="sticky-header">
-            <div class="sticky-minimize"></div>
-            <div class="sticky-close"></div>
-        </div>
-        <div class="sticky-body">
-            <textarea></textarea>
-        </div>
-    `
-    document.body.appendChild(toolSticky);
+    let stickyNoteElem = document.createElement("div");
+    stickyNoteElem.setAttribute("class", "sticky-cont");
+    stickyNoteElem.innerHTML = `
+    <div class="header-cont">
+        <div class="minimize-note"></div>
+        <div class="remove-note"></div>
+    </div>
+    <div class="note-cont">
+        <textarea spellcheck="false"></textarea>
+    </div>
+    `;
 
+    let minimizeNote = stickyNoteElem.querySelector(".minimize-note");
+    let removeNote = stickyNoteElem.querySelector(".remove-note");
+    handleStickyActions(minimizeNote, removeNote, stickyNoteElem);
 
-    let minimize = document.querySelector(".sticky-minimize");
-    let close = document.querySelector(".sticky-close");
-
-
-    // close.addEventListener("click", (e) =>{
-    //     toolSticky.remove();
-    // })
-
-    stickyBody(minimize, close, toolSticky,idNo);
-
-    toolSticky.onmousedown = function(event){
-        dragAndDrop(toolSticky, event);
+    document.body.appendChild(stickyNoteElem);
+    stickyNoteElem.onmousedown = function (event) {
+        dragAndDrop(stickyNoteElem, event);
     };
 
-    toolSticky.ondragstart = function()
-    {
+    stickyNoteElem.ondragstart = function () {
         return false;
     };
 })
 
 
-function stickyBody(minimize, close, tool,idNo)
-{
-    close.addEventListener("click" , (e) => {
-       tool.remove();
-       
+function handleStickyActions(minimizeNote, removeNote, note) {
+    minimizeNote.addEventListener("click", (e) => {
+        stickyFlag = !stickyFlag;
+        let noteCont = note.querySelector(".note-cont");
+        if (stickyFlag) {
+            noteCont.style.display = "block";
+        }
+        else {
+            noteCont.style.display = "none";
+            note.style.height = "2rem";
+        }
     })
-
-    minimize.addEventListener("click", (e) => {
-        let stickyBody = toolSticky.querySelector(".sticky-body");
-        let display = getComputedStyle(stickyBody).getPropertyValue("display");
-        if(display === "none")
-        {
-             stickyBody.style.display = "block";
-        }
-        else
-        {
-            stickyBody.style.display = "none";
-            toolSticky.style.height = "2rem";
-        }
+    removeNote.addEventListener("click", (e) => {
+        note.remove();
     })
 }
 
@@ -139,69 +124,60 @@ upload.addEventListener("click", (e) => {
     input.addEventListener("change", (e) => {
         let file = input.files[0];
         let url = URL.createObjectURL(file);
+        let stickyNoteElem = document.createElement("div");
+        stickyNoteElem.setAttribute("class", "sticky-cont");
+        stickyNoteElem.innerHTML = `
+        <div class="header-cont">
+            <div class="toggle-note"></div>
+            <div class="remove-note"></div>
+        </div>
+        <div class="note-cont">
+            <img src="${url}" />
+        </div>
+        `;
 
-        let toolSticky = document.createElement("div");
-        toolSticky.setAttribute("class", "tool-sticky");
-        toolSticky.innerHTML = `
-            <div class="sticky-header">
-                <div class="sticky-minimize"></div>
-                <div class="sticky-close"></div>
-            </div>
-            <div class="sticky-body">
-                <img src="${url}"/>
-            </div>
-        `
-        document.body.appendChild(toolSticky);
-    
-    
-        let minimize = document.querySelector(".sticky-minimize");
-        let close = document.querySelector(".sticky-close");
-    
-        stickyBody(minimize, close, toolSticky);
-    
-        toolSticky.onmousedown = function(event){
-            dragAndDrop(toolSticky, event);
+        let toggleNote = stickyNoteElem.querySelector(".toggle-note");
+        let removeNote = stickyNoteElem.querySelector(".remove-note");
+        handleStickyActions(toggleNote, removeNote, stickyNoteElem);
+
+        document.body.appendChild(stickyNoteElem);
+        stickyNoteElem.onmousedown = function (event) {
+            dragAndDrop(stickyNoteElem, event);
         };
-    
-        toolSticky.ondragstart = function()
-        {
+
+        stickyNoteElem.ondragstart = function () {
             return false;
-        }; 
-    })
+        };
+    });
 })
 
 
-function dragAndDrop(element, event)
-{
+function dragAndDrop(element, event) {
     let shiftX = event.clientX - element.getBoundingClientRect().left;
     let shiftY = event.clientY - element.getBoundingClientRect().top;
 
-  element.style.position = 'absolute';
-  element.style.zIndex = 1000;
-  
+    element.style.position = 'absolute';
+    element.style.zIndex = 1000;
 
-  moveAt(event.pageX, event.pageY);
-
-  // moves the ball at (pageX, pageY) coordinates
-  // taking initial shifts into account
-  function moveAt(pageX, pageY) {
-    element.style.left = pageX - shiftX + 'px';
-    element.style.top = pageY - shiftY + 'px';
-  }
-
-  function onMouseMove(event) {
     moveAt(event.pageX, event.pageY);
-  }
 
-  // move the ball on mousemove
-  document.addEventListener('mousemove', onMouseMove);
+    // moves the ball at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+        element.style.left = pageX - shiftX + 'px';
+        element.style.top = pageY - shiftY + 'px';
+    }
 
-  // drop the ball, remove unneeded handlers
-  element.onmouseup = function() {
-    document.removeEventListener('mousemove', onMouseMove);
-    element.onmouseup = null;
-  };
+    function onMouseMove(event) {
+        moveAt(event.pageX, event.pageY);
+    }
 
-};
+    // move the ball on mousemove
+    document.addEventListener('mousemove', onMouseMove);
 
-
+    // drop the ball, remove unneeded handlers
+    element.onmouseup = function () {
+        document.removeEventListener('mousemove', onMouseMove);
+        element.onmouseup = null;
+    };
+}
