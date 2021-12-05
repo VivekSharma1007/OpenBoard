9fe26b68aa5d47dda8b088dea8dc94636187004b
VivekSharma1007 <viveksharma10.in@gmail.com>
2021-12-06 00:26:23 +0530

    socket- backend

---
+++ app.js
@@ -0,0 +1,38 @@
+// npm install express
+// npm install socket.io
+// npm init 
+// socket io cdn
+// npm install --save-dev nodemon
+
+const express = require("express");
+const socket = require("socket.io");
+
+const app = express(); // initialize application and server ready
+
+app.use(express.static("public"));
+
+let port = 3000; // to and for motion 8080 5000
+let server = app.listen(port, () => {
+    console.log("Listening to port" + port)
+})
+
+
+let io = socket(server);
+
+io.on("connection", (socket) => {
+    console.log("make socket connection");
+    
+    // received data
+    socket.on("beginPath", (data) => {
+        // transfer data to all connected computers
+        io.sockets.emit("beginPath", data);
+    })
+
+    socket.on("drawStroke", (data) => {
+        io.sockets.emit("drawStroke", data);
+    })
+
+    socket.on("redoundo", (data) => {
+        io.sockets.emit("redoundo", data);
+    })
+})
